a7196800d52aa0f1d269eca5cc86a5c488c8551d
Lucas Alves dos Santos <lucas_ads@hotmail.com.br>
2021-09-09 22:33:45 -0400

    Vue CLI

---
+++ vuejs-cli/src/components/Comments.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+    <div class="form-todo form-group mb-3">
+      <p>
+        <input
+          type="text"
+          placeholder="nome"
+          name="author"
+          class="form-control"
+          v-model="name"
+        />
+      </p>
+      <p>
+        <textarea
+          placeholder="Comentário"
+          name="message"
+          class="form-control"
+          v-model="message"
+        ></textarea>
+      </p>
+      <button v-on:click="addComment" type="submit" class="btn btn-primary">
+        Comentar
+      </button>
+    </div>
+    <div class="list-group">
+      <div class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="comment">
+        <span class="comment_author"
+          >Autor: <strong>{{ comment.name }}</strong></span
+        >
+        <p>{{ comment.message }}</p>
+        <div>
+          <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)"
+            >Excluir</a
+          >
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comments: [
+        {
+          name: "Ayrton",
+          message: "Lorem ipsum",
+        },
+      ],
+      name: "",
+      message: "",
+    };
+  },
+  methods: {
+    addComment() {
+      if (this.message.trim() === "") {
+        return;
+      }
+
+      this.comments.push({
+        name: this.name,
+        message: this.message,
+      });
+      this.name = "";
+      this.message = "";
+    },
+    removeComment(index) {
+      this.comments.splice(index, 1);
+    },
+  },
+  computed: {
+    allComments() {
+      return this.comments.map((comment) => ({
+        ...comment,
+        name: comment.name.trim() === "" ? "Anônimo" : comment.name,
+      }));
+    },
+  },
+  watch: {
+    comments(val) {
+      console.log("val", val);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
